48368c07430931e068bcde82ffff3995e0bf32b5
Yauheni <eugene.kostroma@gmail.com>
2022-01-29 23:58:56 +0400

    lesson12

---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://cdn.pixabay.com/photo/2016/05/24/16/48/mountains-1412683__340.png" alt="mountain" />
+      </div>
+      <div>
+        ava+description
+        <img src="https://i.pinimg.com/originals/b8/5f/58/b85f58e2e1e38407f50ea4e2cf30f08f.png" alt="dog" />
+      </div>
+      <div>
+        my post
+        <div>
+          new post
+        </div>
+        <div>post1</div>
+        <div>post2</div>
+        <div>post3</div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
