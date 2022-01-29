828b5a5af41e188bbbe95afe2d7b2e372c936a35
Yauheni <eugene.kostroma@gmail.com>
2022-01-29 22:18:07 +0400

    lesson9

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Technologies from './Technologies';
 
 function App() {
   return (
     <div>
-      <div>
-        <ul>
-          <li>html</li>
-          <li>css</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header/>
+      <Technologies/>
     </div>
   );
 }
---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+function Technologies() {
+    return (
+      <div>
+          <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>react</li>
+          </ul>
+        </div>
+    )
+  }
+ export default Technologies
