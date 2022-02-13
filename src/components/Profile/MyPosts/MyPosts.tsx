e631f0ac41911416a1ed8b7a91325b37366286ff
Yauheni <eugene.kostroma@gmail.com>
2022-02-13 22:55:09 +0400

    lesson31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import style from './MyPosts.module.css'
 import Post from './Post/Post';
 import {postType} from "../Profile";
@@ -15,9 +15,17 @@ function MyPosts(props:MyPostsType) {
         post=><Post key={post.id} id ={post.id} message={post.message} likeCount={post.likeCount}/>
     )
 
+    let tAreaRef = React.createRef<HTMLTextAreaElement>()
+
     return (
         <div className={style.postsBlock}>
-            <h2>my post</h2>
+            <h2>My posts</h2>
+            <div>
+                <textarea ref={tAreaRef}></textarea>
+                <div>
+                    <button onClick={()=>{alert(tAreaRef.current?.value)}}>add</button>
+                </div>
+            </div>
             <div>
                 <h3>new post</h3>
             </div>
