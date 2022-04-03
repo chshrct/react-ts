6516273b09bbe6d7cf5835b8cf4f07878645b7e9
Yauheni <eugene.kostroma@gmail.com>
2022-04-03 17:01:51 +0400

    fix:refactor user component into class component 53

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,54 +1,62 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 import { UsersPropsType } from "./UsersContainer";
 
-const Users: React.FC<UsersPropsType> = ({users,setUsers,follow,unfollow}) => {
-  useEffect(() => {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        setUsers(response.data.items);
-      });
-  },[users]);
+class Users extends React.Component<UsersPropsType> {
+  componentDidMount() {
+    if (this.props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        });
+    }
+  }
 
-  return (
-    <div>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                src={
-                  u.photos.small !== null
-                    ? u.photos.small
-                    : "https://cdn-icons-png.flaticon.com/512/147/147142.png"
-                }
-                alt="avatar"
-                width={"100px"}
-              />
-            </div>
-            <div>
-              {u.followed ? (
-                <button onClick={() => unfollow(u.id)}>Unfollow</button>
-              ) : (
-                <button onClick={() => follow(u.id)}>Follow</button>
-              )}
-            </div>
-          </span>
-          <span>
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id}>
             <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
+              <div>
+                <img
+                  src={
+                    u.photos.small !== null
+                      ? u.photos.small
+                      : "https://cdn-icons-png.flaticon.com/512/147/147142.png"
+                  }
+                  alt="avatar"
+                  width={"100px"}
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button onClick={() => this.props.unfollow(u.id)}>
+                    Unfollow
+                  </button>
+                ) : (
+                  <button onClick={() => this.props.follow(u.id)}>
+                    Follow
+                  </button>
+                )}
+              </div>
             </span>
             <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
